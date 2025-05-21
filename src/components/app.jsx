57b222c531a34c38f36f16697aa884d67c2f6a6d
DevB4n <buenochaconesteban@gmail.com>
2025-05-21 07:47:00 -0500

    feat: :art: la pagina mas chimba

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
+import { useState } from "react";
+import {motion, AnimatePresence} from 'framer-motion'
+
+
 
 export const Tittles = ({text}) => {
     return (<React.Fragment>
@@ -22,4 +26,93 @@ export const StarterBanner = ({text}) =>{
         </div>
 
     </React.Fragment>)
+}
+
+
+
+export const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState(1);  // 1 = derecha, -1 = izquierda
+
+  const handleNext = () => {
+    setDirection(1); // 1 para dirección derecha
+    setCurrentIndex((prev) => (prev + 1 === images.length ? 0 : prev + 1));
+  };
+
+  const handlePrevious = () => {
+    setDirection(-1); // -1 para dirección izquierda
+    setCurrentIndex((prev) =>
+      prev - 1 < 0 ? images.length - 1 : prev - 1
+    );
+  };
+
+  return (
+    <div className="carousel">
+      <AnimatePresence initial={false}>
+        <motion.img
+          key={currentIndex}
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          initial={{ opacity: 0, x: direction === 1 ? 100 : -100 }}  // Animación de entrada
+          animate={{ opacity: 1, x: 0 }}  // Animación cuando aparece
+          exit={{ opacity: 0, x: direction === 1 ? -100 : 100 }}  // Animación cuando desaparece
+          transition={{ duration: 0.5 }}  // Duración de la transición
+        />
+      </AnimatePresence>
+
+      <div className="slide_direction">
+        <div className="left" onClick={handlePrevious}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+          </svg>
+        </div>
+        <div className="right" onClick={handleNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+          </svg>
+        </div>
+      </div>
+      <div className="indicator">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${currentIndex === index ? "active" : ""}`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const TemplatePhoto = ({PhotoUrl,Tittle, Text}) =>{
+  
+  return(<div className='PhotoContainer'>
+    <video controls  className="Photo">
+      <source src={PhotoUrl} type='video/mp4'></source>
+    </video>
+    <h1 className="PhotoTittle">{Tittle}</h1>
+    <p className='PhotoText'>{Text}</p>
+  </div>)
+}
+
+export const TemplatePhoto2 = ({PhotoUrl,Tittle, Text}) =>{
+  
+  return(<div className='PhotoContainer2'>
+    <video controls className="Photo2">
+      <source src={PhotoUrl} type='video/mp4'></source>
+    </video>
+    <h1 className="PhotoTittle2">{Tittle}</h1>
+    <p className='PhotoText2'>{Text}</p>
+  </div>)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-import { Tittles,StarterBanner} from "./components/app";
+import { TemplatePhoto,TemplatePhoto2,StarterBanner,Carousel} from "./components/app";
+
+
+//videos import
+
+
+import caracoli from '../src/media/videos/caracoli.mp4'
+import noria from '../src/media/videos/noria.mp4'
+import noria_fondo from '../src/media/videos/noria_fondo.mp4'
+import la_flora from '../src/media/videos/la_flora.mp4'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+//carousel images
+
+const images = [
+  "../src/media/imgs/atanasio.jpeg",
+  "../src/media/imgs/cajasan.jpeg",
+  "../src/media/imgs/grado_amor.jpeg",
+  "../src/media/imgs/parque_agua.jpeg",
+  "../src/media/imgs/parque_agua2.jpeg",
+  "../src/media/imgs/parque_florida.jpeg",
+];
+
 root.render(
     <React.Fragment>
-        <StarterBanner text='nuestros mejores momentos'></StarterBanner>
-    </React.Fragment>
+        <StarterBanner text='para mamol'/>
+        <Carousel images={images}/>
+        <TemplatePhoto PhotoUrl={caracoli} Tittle='amiguitos' Text='hola amiguitos' /> 
+        <TemplatePhoto2 PhotoUrl={noria} Tittle='mi foto 2' Text='chimba e texto mi papachoo'/>
+        <TemplatePhoto PhotoUrl={noria_fondo} Tittle='amiguitos' Text='hola amiguitos' /> 
+        <TemplatePhoto2 PhotoUrl={la_flora} Tittle='mi foto 2' Text='chimba e texto mi papachoo'/>
+    </React.Fragment> 
 )
+
+
+
+
