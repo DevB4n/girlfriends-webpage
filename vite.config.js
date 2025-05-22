394f08d48d3cce1b922b6d6b29256afc5496761a
DevB4n <buenochaconesteban@gmail.com>
2025-05-22 11:20:47 -0500

    feat: :art: cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "DevB4n/girlfriends-webpage/", // nombre de tu repositorio en GitHub
+  base: './', // nombre de tu repositorio en GitHub
   plugins: [react()],
 })
+
